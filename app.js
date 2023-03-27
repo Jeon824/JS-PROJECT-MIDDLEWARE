9dec7b68965b64c057c007a6ec90da3c2d7457fd
express-server <roschach824@gmail.com>
2023-03-27 17:22:20 +0900

    rearrange app.js code, set placeholder in routes directory

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express');
-// const dwg = require('./dwg/dwg');
-// const dwg_router = require('./routes/dwg');
-const dwg_router = require('./routes/dwg');
 const app = express();
 const path = require('path');
 // const cassandra = require('cassandra');
 
-app.use('/dwg',dwg_router);
 app.use('/js',express.static(path.join(__dirname,'js')));
 // app.use(express.static(path.join(__dirname,'node_modules')));
 app.use(express.static('frontend'));
